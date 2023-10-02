0c13169627d847fc9c8820b4bae4f71917d9024e
Nimur-Rashid-Imon66 <naimurrashed@gmail.com>
2023-10-03 02:06:50 +0600

    deploy testing

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/Nimur-Rashid-Imon66-IIUCWPS-A14-React-QuizApp/"
 })
